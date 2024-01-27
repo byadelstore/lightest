4112dd0b780107ffe9e42d417faae2d34cd78f2b
byadelstore <byadelstore@gmail.com>
2024-01-27 20:15:40 +0100

    go2

---
+++ .lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['https://www.flashscore.com/'],
+      },
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
+  };
